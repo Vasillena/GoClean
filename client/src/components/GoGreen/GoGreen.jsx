2b77dbbdb09a500acb3e1f2e4d7deb7cfab511f6
Vasillena <vassito@abv.bg>
2023-11-20 14:10:57 +0200

    Fix Go Green page

--- client/src/components/GoGreen/GoGreen.jsx
+++ client/src/components/GoGreen/GoGreen.jsx
@@ -3,68 +3,119 @@ export default function GoGreen() {
     <section
       className="go-green-section"
             style={{
-        background: "url(/images/hero-img.png)",
+        background: "url(/images/hero-img-2.png)",
         backgroundSize: "100% auto",
         backgroundPosition: "center 0",
         backgroundRepeat: "no-repeat",
       }}
     >
       <div className="go-green-text">
-        {/* <h1>
-          Go Green
-        </h1> */}
-        <p>
-          Sustainable Living 101: Discover practical tips to reduce your carbon
+        <h1>
+          Sustainable Living 101
+        </h1>
+        <h3>
+          Discover practical tips to reduce your carbon
           footprint, embrace renewable energy, and make eco-conscious choices in
           your everyday life.
-        </p>
-        <p>
+        </h3>
+        <div className="paragraph-container">
+          <img src="./images/1.jpg" alt="go-green" />
+            <p>
           Waste Reduction Made Easy: Learn simple techniques for recycling,
           composting, and minimizing waste generation, helping you contribute to
           a cleaner and greener environment.
         </p>
+        </div>
+        <div className="trail">
+          <img src="./images/trail.png" alt="trail" />
+        </div>
+            <div className="paragraph-container even">
+          <img src="./images/2.jpg" alt="go-green" />
         <p>
           Energy Conservation Hacks: Explore energy-saving strategies to lower
           your electricity consumption, from optimizing home appliances to
           adopting efficient lighting solutions.
         </p>
+            </div>
+               <div className="trail">
+          <img src="./images/trail-2.png" alt="trail" />
+        </div>
+                <div className="paragraph-container">
+          <img src="./images/3.jpg" alt="go-green" />
         <p>
           Eco-Friendly Transportation: Find eco-conscious alternatives to reduce
           emissions, such as cycling, carpooling, or using public transport, and
           make a positive impact on air quality.
         </p>
+        </div>
+           <div className="trail">
+          <img src="./images/trail.png" alt="trail" />
+        </div>
+            <div className="paragraph-container even">
+          <img src="./images/4.jpg" alt="go-green" />
         <p>
           Green Home Makeover: Get inspired with ideas for sustainable home
           improvements, including eco-friendly materials, water-saving
           techniques, and energy-efficient upgrades.
         </p>
+            </div>
+               <div className="trail">
+          <img src="./images/trail-2.png" alt="trail" />
+        </div>
+                <div className="paragraph-container">
+          <img src="./images/5.jpg" alt="go-green" />
         <p>
           Sustainable Fashion Guide: Learn about ethical fashion choices,
           eco-friendly fabrics, and responsible shopping practices to support a
           more sustainable and fair industry.
         </p>
+          </div>
+             <div className="trail">
+          <img src="./images/trail.png" alt="trail" />
+        </div>
+           <div className="paragraph-container even">
+          <img src="./images/6.jpg" alt="go-green" />
         <p>
           Green Gardening Tips: Discover organic gardening methods, native plant
           selection, and eco-friendly pest control techniques to create a
           flourishing and eco-conscious garden.
         </p>
+          </div>
+             <div className="trail">
+          <img src="./images/trail-2.png" alt="trail" />
+        </div>
+           <div className="paragraph-container">
+          <img src="./images/7.jpg" alt="go-green" />
         <p>
           Water Conservation Strategies: Explore smart water-saving practices
           for both indoor and outdoor use, helping you conserve this precious
           resource and protect ecosystems.
         </p>
+        </div>
+           <div className="trail">
+          <img src="./images/trail.png" alt="trail" />
+        </div>
+              <div className="paragraph-container even">
+          <img src="./images/9.jpg" alt="go-green" />
         <p>
           Mindful Consumption: Uncover strategies to make conscious purchasing
           decisions, reduce single-use plastics, and support businesses
           committed to sustainability.
         </p>
+        </div>
+           <div className="trail">
+          <img src="./images/trail-2.png" alt="trail" />
+        </div>
+              <div className="paragraph-container">
+          <img src="./images/10.jpg" alt="go-green" />
         <p>
           Food for Thought: Dive into sustainable food choices, such as adopting
           a plant-based diet, reducing food waste, and supporting local, organic
           producers for a healthier planet.
         </p>
+         </div>
       </div>
-      <div className="who-are-we-bottom">
+      <div className="go-green-bottom">
         <h2>
           Our path to a <span>greener</span> future starts here...
         </h2>
